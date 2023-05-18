f73f90e3a87fd3b7ae227f6a655afded3b475ce3
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-05-18 01:29:45 -0400

    not using kmeans

--- image_pixel_art/main.py
+++ image_pixel_art/main.py
@@ -5,15 +5,20 @@ from pixelize import Pixelize
 from kmeans import KMeans
 
 def main():
-    img_path = f"{DATA_PATH}/samples/img.jpg"
+    img_path = f"{DATA_PATH}/samples/img.png"
 
     loader = Loader.load(img_path)
+
+    pixelize = Pixelize(pixel_size=50)
+    # kmeans = KMeans(k=25, log=True, max_iterations=10)
+
     alg = CompositeProcessor(
         loader,
-        Pixelize(pixel_size=50),
-        KMeans(k=25, log=True, max_iterations=10),
+        pixelize,
+        # kmeans,
     )
     alg.save_processed_img()
+    # print(alg.calc_error(), pixelize.pixel_num)
 
 if __name__ == "__main__":
     main()
