1f0d196435911499c40349c6901f455b4c05c8d8
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-05-18 01:34:28 -0400

    png to jpg

--- preprocess_large/kmeans.py
+++ preprocess_large/kmeans.py
@@ -57,6 +57,6 @@ class KMeans(Processor):
 
 
 if __name__ == "__main__":
-    loader = Loader.load(f"{DATA_PATH}/samples/img_processed_downscaled.png")
+    loader = Loader.load(f"{DATA_PATH}/samples/img_processed_downscaled.jpg")
     alg = KMeans(loader, k=3, log=True, max_iterations=10)
-    alg.save_processed_img(path=f"{DATA_PATH}/samples/kmeans_img.png")
+    alg.save_processed_img(path=f"{DATA_PATH}/samples/kmeans_img.jpg")
